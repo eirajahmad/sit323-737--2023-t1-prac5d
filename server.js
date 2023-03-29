eb2217a311ec1606176134bc048215b179aacbb6
eirajahmad <46993669+eirajahmad@users.noreply.github.com>
2023-03-30 01:46:57 +1100

    basic api code
    basic api code

--- server.js
+++ server.js
@@ -4,15 +4,57 @@ var port = process.env.port || 3000;
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({extends:false}))
-
+const winston = require('winston');
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'calculate-service' },
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  });
+  
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  //
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
 
 //get api for add
-app.get("/addTwoNumbers", (req,res) => {
-   var number1 = req.query.number1;
-   var number2 = req.query.number2;
-   var result = addNumbers(number1, number2)
-   res.json({statusCode: 200, data: result, message:'Success'})
-})
+app.get("/add", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = addNumbers(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
 const addNumbers = (number1, number2) => {   
     var num1 = parseInt(number1)
     var num2 = parseInt(number2)
@@ -20,18 +62,102 @@ const addNumbers = (number1, number2) => {
     return result;
 }    
 //subtract
-app.get("/subtractTwoNumbers", (req,res) => {
-   var number1 = req.query.number1;
-   var number2 = req.query.number2;
-   var result = subtractNumbers(number1, number2)
-   res.json({statusCode: 200, data: result, message:'Success'})
-})
+app.get("/subtractNumbers", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = subtractNumbers(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
 const subtractNumbers = (number1, number2) => {   
     var num1 = parseInt(number1)
     var num2 = parseInt(number2)
     var result = num1-num2;
     return result;
-}    
+}
+//multiply
+app.get("/multiplyNumbers", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for multiplication');
+    const result = multiplyNumbers(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+const multiplyNumbers = (number1, number2) => {   
+     var num1 = parseInt(number1)
+     var num2 = parseInt(number2)
+     var result = num1*num2;
+     return result;
+ }
+ //divide
+ app.get("/divideNumbers", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for division');
+    const result = divideNumbers(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+const divideNumbers = (number1, number2) => {   
+     var num1 = parseInt(number1)
+     var num2 = parseInt(number2)
+     var result = num2/num1;
+     return result;
+ }  
+      
 var port= process.env.port||3000;
 app.listen(port,()=>{
     console.log("App listening to https://localhost: "+port)
